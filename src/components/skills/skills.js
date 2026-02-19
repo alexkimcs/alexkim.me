18f301a348cef26bde58e433de612c1731a54d04
Alex Kim <alexkimfit@gmail.com>
2026-02-19 12:14:11 -0700

    Update skills

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -2,14 +2,18 @@ import * as React from "react"
 import "./skills.scss"
 
 const skillsData = {
-  "languagesTitle": "Frontend",
-  "languages": ["Python", "Java", "TypeScript", "Kotlin"],
-  "frameworksTitle": "Backend",
-  "frameworks": ["Node.js", "Firebase", "Python", "Java - Spring"],
+  "languagesTitle": "Languages",
+  "languages": ["Python", "Java", "TypeScript", "JavaScript"],
+  "frameworksTitle": "Frameworks",
+  "frameworks": ["Node.js", "Express", "Django", "Prisma"],
+  "platformsTitle": "DevOps",
+  "platforms": ["Ansible","Terraform", "Docker", "Kubernetes"],
+  "testingTitle": "Testing",
+  "testing": ["Appium", "Selenium", "Playwright", "TestNG"],
   "toolsTitle": "Servers",
   "tools": ["Scaling", "Serverless", "Microservice", "Monolith"],
-  "designTitle": "Cloud",
-  "design": ["Firebase", "Google Cloud", "AWS", "Digital Ocean"],
+  "designTitle": "Cloud & OS",
+  "design": ["Linux (RHEL)", "AWS"],
 }
 
 const Skills = () => (
@@ -27,6 +31,12 @@ const Skills = () => (
           <div className="skillz__category__label">{skillsData.frameworksTitle}</div>
           <ul>
             {skillsData.frameworks.map((data) => <li className="skillz__category__item">{data}</li>)}
+          </ul>
+        </div>
+         <div className="skillz__category">
+          <div className="skillz__category__label">{skillsData.platformsTitle}</div>
+          <ul>
+            {skillsData.platforms.map((data) => <li className="skillz__category__item">{data}</li>)}
           </ul>
         </div>
         <div className="skillz__category">
